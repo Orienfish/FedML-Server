33d2b3a989078ad20201ad484d6f9647d1ef96eb
Orienfish <x1yu@ucsd.edu>
2022-06-03 22:52:07 -0700

    fix scirpt

---
+++ executor/run_exp.sh
@@ -0,0 +1,4 @@
+bash ./run_server.sh mnist fedavg iid;
+bash ./run_server.sh mnist fedasync iid;
+bash ./run_server.sh mnist fedavg noniid;
+bash ./run_server.sh mnist fedasync noniid;
--- executor/run_server.sh
+++ executor/run_server.sh
@@ -24,7 +24,7 @@ if [ "$3" = "iid" ] ; then
   python3 app_CNN.py --method "$2" --dataset "$1" --partition_method iid --lr 0.01 --momentum 0.9 \
     --data_size_per_client 600 --client_num_in_total "$TOTAL_CLIENTS" --client_num_per_round "$CLIENTS_PER_ROUND" \
     --comm_round "$rounds" --epochs 5 \
-    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --trial 0
+    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial 0
 fi
 
 # non-iid, all labels from one class on one client
@@ -33,7 +33,7 @@ fi
 #    --partition_alpha 1.0 --partition_secondary --data_size_per_client 600 --lr 0.01 --momentum 0.9 \
 #    --client_num_in_total "$TOTAL_CLIENTS" --client_num_per_round "$CLIENTS_PER_ROUND" \
 #    --comm_round "$rounds" --epochs 5 \
-#    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --trial 0
+#    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial 0
 #fi
 
 # non-iid, all labels from one major class, and uniformally from the rest
@@ -42,5 +42,5 @@ if [ "$3" = "noniid" ] ; then
     --partition_alpha 0.5 --data_size_per_client 600 --lr 0.01 --momentum 0.9 \
     --client_num_in_total "$TOTAL_CLIENTS" --client_num_per_round "$CLIENTS_PER_ROUND" \
     --comm_round "$rounds" --epochs 5 \
-    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --trial 0
+    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial 0
 fi

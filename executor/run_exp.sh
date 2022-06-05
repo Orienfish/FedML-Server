2d7e76f45cc7ef38fc0d9ca56c8376752ca3bfa7
Orienfish <x1yu@ucsd.edu>
2022-06-05 09:49:03 -0700

    fix script

--- executor/run_exp.sh
+++ executor/run_exp.sh
@@ -1,4 +1,9 @@
-bash ./run_server.sh mnist fedavg iid | tee log_fedavg_iid;
-bash ./run_server.sh mnist fedasync iid | tee log_fedasync_iid;
-#bash ./run_server.sh mnist fedavg noniid 1 | tee log_fedavg_noniid;
-#bash ./run_server.sh mnist fedasync noniid 1 | tee log_fedasync_noniid;
+# Usage:
+#   Run FedML Server experiments
+#     bash ./run_exp.sh 0
+#   Last param: trial number
+
+bash ./run_server.sh mnist fedavg iid "$1" | tee log_fedavg_iid;
+bash ./run_server.sh mnist fedasync iid "$1" | tee log_fedasync_iid;
+#bash ./run_server.sh mnist fedavg noniid "$1" | tee log_fedavg_noniid;
+#bash ./run_server.sh mnist fedasync noniid "$1" | tee log_fedasync_noniid;

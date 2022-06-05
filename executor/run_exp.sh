8e0d1759ba1264043ed179a310f48e8f22adc036
Orienfish <x1yu@ucsd.edu>
2022-06-05 09:44:07 -0700

    add log storage to script

--- executor/run_exp.sh
+++ executor/run_exp.sh
@@ -1,4 +1,4 @@
-#bash ./run_server.sh mnist fedavg iid;
-#bash ./run_server.sh mnist fedasync iid;
-bash ./run_server.sh mnist fedavg noniid 1;
-bash ./run_server.sh mnist fedasync noniid 1;
+bash ./run_server.sh mnist fedavg iid | tee log_fedavg_iid;
+bash ./run_server.sh mnist fedasync iid | tee log_fedasync_iid;
+#bash ./run_server.sh mnist fedavg noniid 1 | tee log_fedavg_noniid;
+#bash ./run_server.sh mnist fedasync noniid 1 | tee log_fedasync_noniid;

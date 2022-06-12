4dc626f7687cb7447ad9f3107835c0d0128254ca
Orienfish <x1yu@ucsd.edu>
2022-06-11 20:58:28 -0700

    tune client selection

--- executor/run_exp.sh
+++ executor/run_exp.sh
@@ -1,9 +1,12 @@
 # Usage:
 #   Run FedML Server experiments
-#     bash ./run_exp.sh 0
-#   Last param: trial number
+#     bash ./run_exp.sh
 
-#bash ./run_server.sh mnist fedavg iid "$1" 2>&1 | tee fedavg_iid.log;
-bash ./run_server.sh mnist fedasync iid "$1" 2>&1 | tee fedasync_iid.log;
-#bash ./run_server.sh mnist fedavg noniid "$1" | tee fedavg_noniid.log;
-#bash ./run_server.sh mnist fedasync noniid "$1" | tee fedasync_noniid.log;
+#bash ./run_server.sh mnist fedasync noniid tier "$1" | tee fedasync_noniid.log;
+for sel in random coreset
+do
+  #bash ./run_server.sh mnist fedavg iid "$sel" 0 2>&1 | tee fedavg_iid.log;
+  #bash ./run_server.sh mnist fedasync iid "$sel" 0 2>&1 | tee fedasync_iid.log;
+  bash ./run_server.sh mnist fedavg noniid "$sel" 0 2>&1 | tee fedavg_noniid.log;
+  bash ./run_server.sh mnist fedasync noniid "$sel" 0 2>&1 | tee fedasync_noniid.log;
+done
--- executor/run_server.sh
+++ executor/run_server.sh
@@ -1,16 +1,18 @@
 # Usage:
 #   Run FedML Server
-#     bash ./run_server.sh mnist fedavg noniid 0
+#     bash ./run_server.sh mnist fedavg noniid random 0
 #   Dataset choices: mnist, fashionmnist, cifar10, Shakespeare, Synthetic, HAR, HPWREN
 #   Server choices: fedavg, fedasync
 #   Data distribution choices: iid, noniid
 #     (Note: the Shakespeare, HAR and HPWREN datasets are naturally noniid,
 #     so only noniid selection is available)
+#   Client selection choices: random, high_loss_first, short_latency_first,
+#     short_latency_high_loss_first, divfl, coreset, tier
 #   Last param: trial number
 
 MQTT_HOST=132.239.17.132
 MQTT_PORT=61613
-TOTAL_CLIENTS=10
+TOTAL_CLIENTS=20
 CLIENTS_PER_ROUND=10
 
 # Set proper global rounds
@@ -24,24 +26,15 @@ fi
 if [ "$3" = "iid" ] ; then
   python3 app_CNN.py --method "$2" --dataset "$1" --partition_method iid --lr 0.01 --momentum 0.9 \
     --data_size_per_client 600 --client_num_in_total "$TOTAL_CLIENTS" --client_num_per_round "$CLIENTS_PER_ROUND" \
-    --target_accuracy "$target_acc" --epochs 5 \
-    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial "$4"
+    --target_accuracy "$target_acc" --epochs 5 --selection "$4" \
+    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial "$5"
 fi
 
-# non-iid, all labels from one class on one client
-#if [ "$3" = "noniid" ] ; then
-#  python3 app_CNN.py --method "$2" --dataset "$1" --partition_method noniid --partition_label uniform \
-#    --partition_alpha 1.0 --partition_secondary --data_size_per_client 600 --lr 0.01 --momentum 0.9 \
-#    --client_num_in_total "$TOTAL_CLIENTS" --client_num_per_round "$CLIENTS_PER_ROUND" \
-#    --comm_round "$rounds" --epochs 5 \
-#    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial "$4"
-#fi
-
-# non-iid, all labels from one major class and equally from the rest classes
+# non-iid, all labels equally from a random number of classes between 1 and 5
 if [ "$3" = "noniid" ] ; then
   python3 app_CNN.py --method "$2" --dataset "$1" --partition_method noniid --partition_label uniform \
-    --partition_alpha 0.5 --data_size_per_client 600 --lr 0.01 --momentum 0.9 \
+    --partition_min_cls 1 --partion_max_cls 5 --data_size_per_client 600 --lr 0.01 --momentum 0.9 \
     --client_num_in_total "$TOTAL_CLIENTS" --client_num_per_round "$CLIENTS_PER_ROUND" \
-    --target_accuracy "$target_acc" --epochs 5 \
-    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial "$4"
+    --target_accuracy "$target_acc" --epochs 5 --selection "$4" \
+    --backend MQTT --mqtt_host "$MQTT_HOST" --mqtt_port "$MQTT_PORT" --server_ip "$MQTT_HOST" --trial "$5"
 fi
